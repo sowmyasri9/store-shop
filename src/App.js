2cd679c27a17691495603d5f9769f646c93862c5
sowmyasri9 <sowmyasri23164@gmail.com>
2021-08-07 00:43:28 +0530

    Implemented firebase utils, included ability to store authenticated users into firebase database

--- src/App.js
+++ src/App.js
@@ -7,29 +7,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import {auth} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      currentUser:null
+    this.state = {
+      currentUser: null
     }
   }
-unsubscribeFromAuth=null;
-  componentDidMount(){
-    this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
-      this.setState({currentUser:user});
-      console.log(user);
-    })
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+      else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
   }
-  componentWillUnmount(){
+
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  render(){
+  render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -38,7 +52,7 @@ unsubscribeFromAuth=null;
       </div>
     );
   }
-  
+
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,23 +2,46 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config={
-    apiKey: "AIzaSyDpkhbsyUdECWRmjTu7j77VnWym4M6MpuQ",
-    authDomain: "store-shop-db.firebaseapp.com",
-    projectId: "store-shop-db",
-    storageBucket: "store-shop-db.appspot.com",
-    messagingSenderId: "813191857653",
-    appId: "1:813191857653:web:01f22de8a259a3cbe3ccbc",
-    measurementId: "G-KQFV22677F"
+const config = {
+  apiKey: "AIzaSyDpkhbsyUdECWRmjTu7j77VnWym4M6MpuQ",
+  authDomain: "store-shop-db.firebaseapp.com",
+  projectId: "store-shop-db",
+  storageBucket: "store-shop-db.appspot.com",
+  messagingSenderId: "813191857653",
+  appId: "1:813191857653:web:01f22de8a259a3cbe3ccbc",
+  measurementId: "G-KQFV22677F"
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
   }
+return userRef;
+}
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  firebase.initializeApp(config);
-  export const auth=firebase.auth();
-  export const firestore=firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
 
-  const provider=new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt:'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  export const signInWithGoogle=()=>auth.signInWithPopup(provider);
-
-  export default firebase;
+export default firebase;
