c8a0e10b5ef0e04d49ec260df06cd81e7a555b8c
sowmyasri9 <sowmyasri23164@gmail.com>
2021-08-12 20:01:53 +0530

    removed console log

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,7 +6,6 @@ import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({match}) => 
 {
-    console.log(match)
     return(
     <div className='shop-page'>
      <Route exact path={`${match.path}`} component={CollectionsOverview}/>
